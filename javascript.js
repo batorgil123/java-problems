5a9300a4d579ae9c5bd05ba4573660cf0273f977
IHAW <imalreadylost9@gmail.com>
2024-12-03 20:29:13 -0800

    sex

---
+++ javascript.js
@@ -0,0 +1,436 @@
+// // problem-1
+// let a = 5,
+//   b = 4,
+//   c = 3;
+// let p = a + b + c;
+// console.log(p);
+
+// // problem-2
+// let a2 = 12,
+//   b2 = 23;
+// console.log(a2 + b2);
+
+// // problem-3
+// let a3 = 5;
+// console.log(a3 * a3 * a3, a3 * a3 * 6);
+
+// // problem-4
+// let a4 = 6,
+//   b3 = 4;
+// console.log(a4 * b3, (a4 + b3) * 2);
+
+// // problem-5
+// let x = 1;
+// console.log(3 * x - 5);
+
+// // problem-6
+// let x2 = -2;
+// console.log(4 * x2 * x2 - 3 * x2 + 5);
+
+// // problem-7
+// let a5 = 476;
+// console.log(a5 % 10);
+
+// // problem-8
+// let n = 423;
+// console.log(Math.floor(n / 10) % 10);
+
+// // problem-9
+// let m = 27;
+// console.log(Math.floor(m / 10) * (m % 10));
+
+// // problem-10
+// let m2 = 200;
+// console.log(Math.floor(m2 / 60), m2 % 60);
+
+// // problem-11
+// let n2 = 3860;
+// let hour = Math.floor(n2 / 3600),
+//   minut = Math.floor((n2 % 3600) / 60),
+//   sec = (n2 % 3600) % 60;
+// console.log(hour, minut, sec);
+
+// // problem-12
+// let min = 3,
+//   sec2 = 4;
+// console.log(min * 60 + sec2);
+
+// // problem-13
+// let hour2 = 1,
+//   min2 = 2,
+//   sec3 = 3;
+// console.log(hour2 * 3600 + min2 * 60 + sec3);
+
+// // problem-14
+// let hour3 = 44;
+// console.log(Math.floor(hour3 / 24), hour3 % 24);
+
+// // problem-15
+// let day = 2,
+//   hour4 = 5;
+// console.log(day * 24 + hour4);
+
+// // problem-16
+// let month = 27;
+// console.log(Math.floor(month / 12), month % 12);
+
+// // problem-17
+// let year = 2,
+//   month2 = 7;
+// console.log(year * 12 + month2);
+
+// // problem-18
+// let number1 = 16,
+//   number2 = 14;
+// if (number1 >= number2) console.log(number1);
+// else console.log(number2);
+
+// // problem-19
+// let number3 = 9,
+//   number4 = 6;
+// if (number3 >= number4) console.log(number4);
+// else console.log(number3);
+
+// // problem-20
+// let a6 = 1,
+//   a7 = 2,
+//   a8 = 3,
+//   answer;
+// if (a6 >= a7) answer = a6;
+// else answer = a7;
+// if (a8 > answer) answer = a8;
+// console.log(answer);
+
+// // problem-21
+// let x3 = 3,
+//   x4 = 2,
+//   x5 = 1,
+//   x6 = 4;
+// let answer2 = x3;
+// if (x3 >= x4) answer = x4;
+// else answer = x3;
+// if (x5 < answer2) answer2 = x5;
+// if (x6 < answer2) answer2 = x6;
+// console.log(answer2);
+
+// // problem-22
+// let r1 = 85,
+//   r2 = 75,
+//   r3 = 96,
+//   r4 = 69,
+//   s = 0;
+// if (r1 > 80) s += r1;
+// if (r2 > 80) s += r2;
+// if (r3 > 80) s += r3;
+// if (r4 > 80) s += r4;
+// console.log(s);
+
+// // problem-23
+// let t1 = 3,
+//   t2 = 6,
+//   t3 = 2,
+//   t4 = 4,
+//   s2 = 1;
+// if (t1 < 5) s2 *= t1;
+// if (t2 < 5) s2 *= t2;
+// if (t3 < 5) s2 *= t3;
+// if (t4 < 5) s2 *= t4;
+// console.log(s2);
+
+// // problem-24
+// let v1 = 10,
+//   v2 = 3,
+//   v3 = 5,
+//   s3 = 0;
+// if (v1 % 2 == 0) s3 += v1;
+// if (v2 % 2 == 0) s3 += v2;
+// if (v3 % 2 == 0) s3 += v3;
+// console.log(s3);
+
+// // problem-25
+// let c1 = 5,
+//   c2 = 2,
+//   c3 = 3,
+//   s4 = 1;
+// if (c1 % 2 == 1) s4 *= c1;
+// if (c2 % 2 == 1) s4 *= c2;
+// if (c3 % 2 == 1) s4 *= c3;
+// console.log(s4);
+
+// // problem-25
+// let b6 = 3,
+//   b7 = 12,
+//   b8 = 8,
+//   b9 = 9,
+//   s6 = 0;
+// if (b6 % 3 == 0) s6++;
+// if (b7 % 3 == 0) s6++;
+// if (b8 % 3 == 0) s6++;
+// if (b9 % 3 == 0) s6++;
+// console.log(s6);
+
+// // problem-26
+// let q1 = 7,
+//   q2 = 22,
+//   q3 = 13,
+//   q4 = 30,
+//   s7 = 0;
+// if (q1 % 11 != 0) s7 += q1;
+// if (q2 % 11 != 0) s7 += q2;
+// if (q3 % 11 != 0) s7 += q3;
+// if (q4 % 11 != 0) s7 += q4;
+// console.log(s7);
+
+// // problem-27
+// let n3 = 11;
+// if (n3 > 10) console.log("YES");
+// else console.log("NO");
+
+// // problem-28
+// let n4 = 4;
+// if (n4 < 5) console.log("YES");
+// else console.log("NO");
+
+// // problem-29
+// let w1 = 3,
+//   w2 = 4,
+//   w3 = 5;
+// if (w1 % 2 == 0) console.log("YES");
+// else console.log("NO");
+// if (w2 % 2 == 0) console.log("YES");
+// else console.log("NO");
+// if (w3 % 2 == 0) console.log("YES");
+// else console.log("NO");
+
+// // problem-30
+// console.log("IOI");
+
+//  problem-31
+// console.log("IOI");
+// console.log("IOI");
+// console.log("IOI");
+
+// // problem-32
+// let n5 = 4;
+// for (let i = 1; i <= n5; i++) {
+//   console.log("IOI");
+// }
+
+// // problem-33
+// let N = 4;
+// console.log(((N + 1) * N) / 2);
+
+// // problem-34
+// let N2 = 5,
+//   fac = 1;
+// for (let j = 1; j <= N2; j++) fac = fac * j;
+// console.log(fac);
+
+// // problem-35
+// let N3 = 6, S = 0;
+// for (let i = 1; i <= N3; i++) S += 100;
+// console.log(S);
+
+// // problem-36
+// let N4 = 6,
+//   S2 = 0,
+//   A = 50;
+// for (let i = 1; i <= N4; i++) S2 += A;
+// console.log(S2);
+
+// // problem-37
+// let N5 = 5;
+// console.log(Math.pow(2, 5));
+
+// // problem-38
+// let N6 = 3,
+//   M = 4;
+// console.log(Math.pow(N6, M));
+
+// // problem-39
+// let N7 = 4;
+// if (N7 > 3) console.log("tentssen");
+
+// // problem-40
+// let N8 = 4;
+// if (N8 == 5) console.log("Onts");
+// if (N8 == 4) console.log("Sain");
+// if (N8 == 3) console.log("Dund");
+// if (N8 == 2) console.log("Muu");
+
+// // problem-41
+// let N9 = 84;
+// if (N9 > 89) console.log("A");
+// if (N9 <= 89 && N9 > 79) console.log("B");
+// if (N9 <= 79 && N9 > 69) console.log("C");
+// if (N9 <= 69 && N9 > 59) console.log("D");
+// if (N9 <= 59) console.log("F");
+
+// // problem-42
+// let M3 = 5;
+// if (M3 == 1) console.log("Monday");
+// if (M3 == 2) console.log("Tuesday");
+// if (M3 == 3) console.log("Wednesday");
+// if (M3 == 4) console.log("Thursday");
+// if (M3 == 5) console.log("Friday");
+// if (M3 == 6) console.log("Saturday");
+// if (M3 == 7) console.log("Sunday");
+
+// // problem-43
+// let tal1 = 3,
+//   tal2 = 4,
+//   tal3 = 5;
+// if (tal1 + tal2 > tal3 && tal1 + tal3 > tal2 && tal2 + tal3 > tal1)
+//   console.log("YES");
+// else console.log("NO");
+
+// // problem-44
+// let M4 = 3;
+// for (let i = 1; i <= 9; i++) console.log(M4, "*", i, "=", M4 * i);
+
+// // problem-45
+// let   A4=3,M5=5;
+// for(let i=1;i<=M5;i++)
+// console.log(A4,'*');
+// */
+// //JS - IF additional problems
+
+// //Тэгш, сондгой тоог тодорхойлох
+// let n = 7;
+// if (n % 2 == 0) console.log("Tegsh");
+// else console.log("Sondgoi");
+
+// //Тоог эерэг, сөрөг, тэг байдлаар ангилах
+// let n2 = -3;
+// if (n2 < 0) console.log("Surug");
+// if (n2 == 0) console.log("teg");
+// if (n2 > 0) console.log("Eyreg");
+
+// //Нэг тоо нөгөө тоогоор хуваагддаг эсэхийг шалгах
+// let n3 = 3,
+//   m = 4;
+// if (n3 % m == 0) console.log("YES");
+// else console.log("NO");
+
+// //Тоон оноог үсгийн дүн (A, B, C, D, F) болгон хувиргах дүнгийн тооцоолуур
+// let N9 = 84;
+// if (N9 > 89) console.log("A");
+// if (N9 <= 89 && N9 > 79) console.log("B");
+// if (N9 <= 79 && N9 > 69) console.log("C");
+// if (N9 <= 69 && N9 > 59) console.log("D");
+// if (N9 <= 59) console.log("F");
+
+// //Цельсийг Фаренгейтын температурт хувиргах
+// //let n4 = prompt();
+// //console.log(n4 * 1.8 + 32);
+
+// // Өгөгдсөн жил нь өндөр жил эсэхийг шалгах
+// let n7 = prompt(Number);
+// if (Math.abs(n7 - 2024) % 4 == 0 && n7%400==0) console.log("YES");
+// else console.log("NO");
+
+// Өгөгдсөн гурван урт нь гурвалжин үүсгэх боломжтой эсэхийг шалгах
+// let tal1 = 3;
+// let tal2 = 4;
+// let tal3 = 5;
+// console.log(tal1, tal2, tal3);
+// if (tal1 + tal2 > tal3 && tal1 + tal3 > tal2 && tal2 + tal3 > tal1)
+//   console.log("YES");
+// else console.log("NO");
+
+// Тоо төгс тоо эсэхийг шалгах
+// let N10 = prompt(Number),sum = 0;
+// if (N10 == 6) console.log("YES");
+// else {
+//   if (N10 < 6) console.log("NO");
+//   else {
+//     let p = 3,k = 0;
+//     while (Math.pow(2, p - 1) * (Math.pow(2, p) - 1) <= N10) {
+//       if (Math.pow(2, p - 1) * (Math.pow(2, p) - 1) == N10) {
+//         console.log("YES");
+//         k = 1;
+//         break;
+//       }
+//       p += 2;
+//     }
+//     if (k == 0) console.log("NO");
+//   }
+// }
+
+// Тэмдэгт эгшиг эсвэл гийгүүлэгч эсэхийг шалгах
+// let N11 = prompt(String);
+// if (
+//   N11 == "а" ||
+//   N11 == "э" ||
+//   N11 == "о" ||
+//   N11 == "и" ||
+//   N11 == "у" ||
+//   N11 == "ө" ||
+//   N11 == "ү" ||
+//   N11 == "А" ||
+//   N11 == "Э" ||
+//   N11 == "И" ||
+//   N11 == "О" ||
+//   N11 == "У" ||
+//   N11 == "Ө" ||
+//   N11 == "Ү"
+// )
+//   console.log("Egshig");
+// else console.log("Giiguulegch");
+
+// 1. Өгөгдсөн тоонд өгөгдсөн цифр хэдэн удаа орсныг ол.
+// let n = prompt(Number),
+//   m = prompt(Number),
+//   k = 0;
+
+// while (n > 0) {
+//   if (n % 10 == m) k++;
+//   n = Math.floor(n / 10);
+// }
+// console.log(k);
+
+// 2. Өгөгдсөн хоёр натурал тооны хамгийн их ерөнхий хуваагчийг ол.
+// let a = prompt(Number),
+//   b = prompt(Number);
+// while (a > 0 && b > 0) {
+//   if (a > b) a = a % b;
+//   else b = b % a;
+// }
+// if (a > 0) console.log(a);
+// else console.log(b);
+
+// 3. Элсэлтийн ерөнхий шалгалтын (ЭЕШ) математикийн дүнг сурагчдын хувийн дугаараар нь жагсаан гаргасан байгаа. Өндөр оноотой сурагчдыг эхэнд нь жагсааж бич. Оноо тэнцсэн тохиолдолд хувийн дугаар багатайг нь эхэнд нь жагсаана. Сурагчдын хувийн дугаар 1, 2, 3, гэх мэтээр үргэлжлэн олгогдсон байгаа.
+// let n = prompt(Number),
+//   a = Array(100),
+//   k = 0,
+//   j = 0,
+//   i = 0;
+// let c = Array(100);
+// for (i = 1; i <= n; i++) {
+//   a[i] = prompt() * 1;
+//   c[i] = i;
+// }
+// for (i = 1; i < n; i++) {
+//   for (j = i + 1; j <= n; j++) {
+//     if (a[i] > a[j]) {
+//       //bair solih
+//       k = a[j];
+//       a[j] = a[i];
+//       a[i] = k;
+//       //index solih
+//       k = c[j];
+//       c[j] = c[i];
+//       c[i] = k;
+//     }
+//     if(a[i]==a[j])
+//     {
+//       k = c[j];
+//       c[j] = c[i];
+//       c[i] = k;
+//     }
+//   }
+// }
+// console.log(c);
+
+// 4. Өгөгдсөн тоо палиндром бол YES үгүй бол NO гэж хэвлэ. Палиндром тоо гэдэг нь тонгорогсон тоотойгоо тэнцүү тоог хэлнэ.
+// let n=prompt(Number);
